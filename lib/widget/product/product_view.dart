ea8507b6dd7ab7931247a7f0b3d4f51118eacf1d
michaelandom03 <michaelandom03@gmail.com>
2021-11-22 12:16:58 +0300

    detail update

--- lib/widget/product/product_view.dart
+++ lib/widget/product/product_view.dart
@@ -52,7 +52,7 @@ class JobView extends StatefulWidget {
               return Text(
                 detailPage
                     ? "${job.jobChannel!.name![0].toUpperCase()}${job.jobChannel!.name![1].toUpperCase()}"
-                    : "${job.company!.name![0].toUpperCase()} ${job.company!.name![1].toUpperCase()}",
+                    : "${job.company!.name![0].toUpperCase()}${job.company!.name![1].toUpperCase()}",
                 style: const TextStyle(
                   fontSize: 20,
                   color: CustomColor.TEXT_COLOR_GRAY,
