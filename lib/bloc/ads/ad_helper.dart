c5207cab26462912c786294e405c25b1c6b07e00
Nathan <nathanmersha@gmail.com>
2022-01-14 11:52:21 +0300

    Changed app id and ad unit id for ad mob

--- lib/bloc/ads/ad_helper.dart
+++ lib/bloc/ads/ad_helper.dart
@@ -4,18 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
+
   Future<InitializationStatus> initialized;
   AdHelper({required this.initialized});
   String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // Test Key : ca-app-pub-3940256099942544/6300978111
-      // Production Key : ca-app-pub-6460693144580555/5748323615
-
-      return 'ca-app-pub-6460693144580555/5748323615';
+      String testAdId = "ca-app-pub-3940256099942544/6300978111";
+      String prodAdId = "ca-app-pub-6460693144580555/5059939777";
+      return testAdId;
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-7439240901787604/2533999775';
+      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
     } else {
-      throw new UnsupportedError('Unsupported platform');
+      throw UnsupportedError('Unsupported platform');
     }
   }
 
@@ -25,15 +25,16 @@ class AdHelper {
     } else if (Platform.isIOS) {
       return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
     } else {
-      throw new UnsupportedError('Unsupported platform');
+      throw UnsupportedError('Unsupported platform');
     }
   }
 
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      // Test Key : ca-app-pub-3940256099942544/5224354917
-      // Production Key : ca-app-pub-6460693144580555/3064635740
-      return 'ca-app-pub-6460693144580555/3064635740'; 
+      String testAdId = "ca-app-pub-3940256099942544/5224354917";
+      String prodAdId = "ca-app-pub-6460693144580555/7969351757";
+
+      return testAdId;
     } else if (Platform.isIOS) {
       return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
     } else {
