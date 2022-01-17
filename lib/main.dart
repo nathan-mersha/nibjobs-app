433b872958138b40e3ffcb836a5e14ecf6722adc
michaelandom03 <michaelandom03@gmail.com>
2022-01-17 13:32:34 +0300

    overflow fix

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(backgroundMessage);
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => MyApp(), // Wrap your app
     ),
   );
